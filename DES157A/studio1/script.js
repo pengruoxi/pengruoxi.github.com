498d759dc35f0a8d52d3e6df5ba7274bb6ba7168
Ruoxi Peng <ruopeng@ucdavis.edu>
2021-04-27 09:46:17 -0700

    studio 1

--- DES157A/studio1/script.js
+++ DES157A/studio1/script.js
@@ -37,22 +37,38 @@
 
     const sc1Tag = document.querySelector('#sc1');
     const sc2Tag = document.querySelector('#sc2');
+    const name = document.querySelector('#name');
+    const description = document.querySelector('#description');
+    const occupation = document.querySelector('#occupation');
+    const background = document.querySelector('#background');
     const atbName = ["Strength","Constitution","Dexterity","Size","Intelligence","Power","Appearance","Education"];
+
+    //change elements when change pages
     document.querySelector('.towardTwo').addEventListener('click', function(event){
         for(let i=0;i<attributes.length;i++){
             const newP = document.createElement('p');
-            const pText = document.createTextNode(`Your ${atbName[i]} is ${attributes[i].value}`);
+            const pText = document.createTextNode(`The character's ${atbName[i]} is ${attributes[i].value}`);
             newP.appendChild(pText);
             sc1Tag.appendChild(newP);
         }
 
-        const name = document.getElementById('#name');
-        const age = document.getElementById('#age');
-        const occupation = document.getElementById('#occupation');
-        const time = document.getElementById('#time');
-        const pText2 = document.createTextNode(`${name.value} is a ${age.value}-year-old ${occupation.value}. And the character's story happend in ${time.value}`);
-        sc2Tag.appendChild(pText2);
+        const newP = document.createElement('p');
+        const pText = document.createTextNode(`The character's name is ${name.value}. People will said that he/she is a ${description.value} person. And now he/she is working as a ${occupation.value}. ${background.value}`);
+        newP.appendChild(pText);
+        sc2Tag.appendChild(newP);
+        console.log(name.value);
     });
-    
+
+    //delete useless elements when change pages
+    document.querySelector('.towardOne').addEventListener('click', function(event){
+        let pAmount = attributes.length;
+        while(pAmount > 0){
+            pAmount--;
+            var allP = document.querySelectorAll('#sc1 p');
+            sc1Tag.removeChild(sc1Tag.children[allP.length-1]);
+        }
+        sc2Tag.removeChild(sc2Tag.children[0]);
+    });
+
 
 })();
