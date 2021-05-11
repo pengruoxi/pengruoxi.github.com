8aff3794031b064071c75f164bcfbb48b90b16d5
Ruoxi Peng <ruopeng@ucdavis.edu>
2021-05-11 04:00:28 -0700

    update studio 2

---
+++ DES157A/studio2/script.js
@@ -0,0 +1,52 @@
+(function () {
+    "use strict";
+    console.log("reading js");
+
+    //choose img
+    const myImages = document.querySelectorAll("#one > img");
+
+    myImages.forEach(function (eachImg) {
+        eachImg.addEventListener('mouseover', function(){
+            eachImg.src=`image/${eachImg.id}-selected.png`;
+        });
+        eachImg.addEventListener('mouseout', function(){
+            eachImg.src=`image/${eachImg.id}.png`;
+        });
+    });
+
+    //change page
+
+    const showZeroBtn = document.querySelector('.showZero');
+    const showOneBtn = document.querySelectorAll('.showOne');
+    const showTwoBtn = document.querySelector('.showTwo');
+    showZeroBtn.addEventListener('click', function(){
+        document.getElementById('zero').className = "page showing";
+        document.getElementById('one').className = "page hidden";
+    });
+    
+    showOneBtn.forEach(function(eachBtn){
+        eachBtn.addEventListener('click', function(){
+            document.getElementById('zero').className = "page hidden";
+            document.getElementById('one').className = "page showing";
+            document.getElementById('two').className = "page hidden";
+        });
+    })
+
+    showTwoBtn.addEventListener('click', function(){
+        document.getElementById('one').className = "page hidden";
+        document.getElementById('two').className = "page showing";
+    });
+
+    //change image by time
+    let currentImage = 0;
+    const myPhotos = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"];
+
+    const slide = document.getElementById('myimage');
+
+        setInterval(function(){
+            currentImage++;
+            if(currentImage > myPhotos.length-1){currentImage = 0;}
+            slide.src = `image/${myPhotos[currentImage]}`;
+        },2000);
+    
+})();
